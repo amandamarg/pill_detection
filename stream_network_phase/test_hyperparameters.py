56b6b0450db908610e5d97f3283c77d21bb915d8
amandamarg <ammargo@umich.edu>
2025-07-03 00:04:02 -0400

    added init and fit to estimator

--- stream_network_phase/test_hyperparameters.py
+++ stream_network_phase/test_hyperparameters.py
@@ -21,30 +21,39 @@ from dataloader_stream_network import DataLoaderStreamNet
 from utils.utils import (create_dataset, create_timestamp, get_embedded_text_matrix, measure_execution_time,
                          use_gpu_if_available, setup_logger, load_config_json)
 from train_stream_network import TrainModel
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader, Subset
 
 
-cfg = (
-        load_config_json(
-            json_schema_filename=json_config_selector("stream_net").get("schema"),
-            json_filename=json_config_selector("stream_net").get("config")
-        )
-    )
 
 class ExpirimentModel(TrainModel):
-    def __init__(self, dataset, stream_type, loss_type, epochs, batch_size, weight_decay, step_size, learning_rate, gamma, margin, mining_type, upper_norm_limit=None):
+    def __init__(self, cfg, mapping, train_data_loader, valid_data_loader):
+        self.cfg = cfg
+
         # Set up logger
         self.logger = setup_logger()
 
         # Set up tqdm colour
         colorama.init()
 
+        # Create time stamp
+        self.timestamp = create_timestamp()
+
         # Select the GPU if possible
         self.device = use_gpu_if_available()
 
-        self.type_of_net = cfg.get("type_of_net")
 
-        substream_network_cfg = cfg.get("streams").get(stream_type)
-        backbone_network_cfg = cfg.get("networks").get(self.type_of_net)
+        # Setup network config
+        self.dataset_type = self.cfg.get("dataset_type")
+
+        stream_type = self.cfg.get("type_of_stream")
+        self.type_of_net = self.cfg.get("type_of_net")
+
+        substream_network_cfg = self.cfg.get("streams").get(stream_type)
+        backbone_network_cfg = self.cfg.get("networks").get(self.type_of_net)
+
+        # Loss type
+        loss_type = self.cfg.get("type_of_loss_func")
 
         # Load model and upload it to the GPU
         self.model = StreamNetworkFactory.create_network(self.type_of_net, substream_network_cfg)
@@ -60,32 +69,26 @@ class ExpirimentModel(TrainModel):
             )
         )
 
-        self.mapping = dataset.reference_encoding_map
-        self.train_data_loader, self.valid_data_loader = (
-            create_dataset(
-                dataset=dataset,
-                train_valid_ratio=cfg.get("train_valid_ratio"),
-                batch_size=batch_size
-            )
-        )
+        self.mapping = mapping
 
-        # Set up loss and mining functions
+        self.train_data_loader = train_data_loader
+
+        self.valid_data_loader = valid_data_loader
+
+# Set up loss and mining functions
         if loss_type == "dmtl":
             path_to_excel_file = nlp_configs().get("vector_distances")
             df = get_embedded_text_matrix(path_to_excel_file)
 
-            #TODO: raise error if no upper_norm_limit passed
-
             self.criterion = (
                 DynamicMarginTripletLoss(
-                    margin=margin,
+                    margin=self.cfg.get("margin"),
                     triplets_per_anchor="all",
                     euc_dist_mtx=df,
-                    upper_norm_limit=upper_norm_limit,
+                    upper_norm_limit=self.cfg.get("upper_norm_limit"),
                     mapping_table=self.mapping
                 )
             )
-
         elif loss_type == "hmtl":
             self.criterion = (
                 losses.TripletMarginLoss(
@@ -94,11 +97,11 @@ class ExpirimentModel(TrainModel):
             )
         else:
             raise ValueError(f"Wrong loss function: {loss_type}")
-        
+
         self.mining_func = (
             miners.TripletMarginMiner(
-                margin=margin,
-                type_of_triplets=mining_type
+                margin=self.cfg.get("margin"),
+                type_of_triplets=self.cfg.get("mining_type")
             )
         )
 
@@ -106,26 +109,76 @@ class ExpirimentModel(TrainModel):
         self.optimizer = (
             torch.optim.Adam(
                 self.model.parameters(),
-                lr=learning_rate
+                lr=backbone_network_cfg.get("learning_rate")
             )
         )
-
 
         # LR scheduler
         self.scheduler = (
             StepLR(
                 optimizer=self.optimizer,
-                step_size=step_size,
-                gamma=gamma
+                step_size=self.cfg.get("step_size"),
+                gamma=self.cfg.get("gamma")
             )
         )
 
-        
+        # Tensorboard
+        tensorboard_log_dir = (
+            self.create_save_dirs(
+                network_cfg=substream_paths().get(stream_type),
+                subdir="logs_dir",
+                loss=loss_type
+            )
+        )
+        self.writer = (
+            SummaryWriter(
+                log_dir=tensorboard_log_dir
+            )
+        )
+
+        # Create save directory for model weights
+        self.save_path = (
+            self.create_save_dirs(
+                network_cfg=substream_paths().get(stream_type),
+                subdir="model_weights_dir",
+                loss=loss_type
+            )
+        )
+
+        # Create save directory for hard samples
+        self.hard_samples_path = (
+            self.create_save_dirs(
+                network_cfg=substream_paths().get(stream_type),
+                subdir="hardest_samples",
+                loss=loss_type
+            )
+        )
+
+        # Variables to save only the best weights and model
+        self.best_valid_loss = float('inf')
+        self.best_model_path = None
+    
+    def create_save_dirs(self, network_cfg, subdir, loss) -> str:
+
+        directory_path = network_cfg.get(self.dataset_type).get(self.type_of_net).get(subdir).get(loss)
+        directory_to_create = (
+            os.path.join(directory_path, "expiriments", f"{self.timestamp}")
+        )
+        os.makedirs(directory_to_create, exist_ok=True)
+        return directory_to_create
+
 
 
 
 class Estimator:
-    def __init__(self, epochs, batch_size, weight_decay, step_size, learning_rate, gamma, margin, mining_type, upper_norm_limit,):
+    def __init__(self, epochs, batch_size, weight_decay, step_size, learning_rate, gamma, margin, mining_type, upper_norm_limit=None):
+        self.cfg = (
+            load_config_json(
+                json_schema_filename=json_config_selector("stream_net").get("schema"),
+                json_filename=json_config_selector("stream_net").get("config")
+            )
+        )
+        
         self.epochs = epochs
         self.batch_size = batch_size
         self.weight_decay = weight_decay
@@ -136,13 +189,59 @@ class Estimator:
         self.mining_type = mining_type
         self.upper_norm_limit = upper_norm_limit
 
-        # Select the GPU if possible
-        self.device = use_gpu_if_available()
+        self.datasets = {}
 
+        for stream_type in ["Contour", "LBP", "RGB", "Texture"]:
+
+            # Create dataset
+            dataset_dirs_anchor = (
+                substream_paths().get(stream_type).get(self.dataset_type).get(self.type_of_net).get("train").get("anchor")
+            )
+            dataset_dir_pos_neg = (
+                substream_paths().get(stream_type).get(self.dataset_type).get(self.type_of_net).get("train").get("pos_neg")
+            )
+            
+            dataset = \
+                DataLoaderStreamNet(
+                    dataset_dirs_anchor=[dataset_dirs_anchor],
+                    dataset_dirs_pos_neg=[dataset_dir_pos_neg]
+                )
+            
+            self.datasets[stream_type] = dataset
+            
 
     def fit(self, X, y=None):
         
-        None
-    
+        for key, val in self.get_params():
+            self.cfg[key] = val
+
+        for stream_type in ["Contour", "LBP", "RGB", "Texture"]:
+            self.cfg["type_of_stream"] = stream_type
+            dataset = self.datasets[stream_type]
+            mapping = dataset.reference_encoding_map
+            indicies = [dataset.reference_labels.index(label) for label in X]
+            train_data_loader, valid_data_loader = (
+                create_dataset(
+                    dataset=Subset(dataset, indicies),
+                    train_valid_ratio=self.cfg.get("train_valid_ratio"),
+                    batch_size=self.cfg.get("batch_size")
+                )
+            )
+            
+            model = ExpirimentModel(self.cfg, mapping, train_data_loader, valid_data_loader)
+            try:
+                tm = model()
+                try:
+                    model.training()
+                except torch.cuda.OutOfMemoryError:
+                    logging.error('Detected OutOfMemoryError!')
+                    torch.cuda.empty_cache()
+            except KeyboardInterrupt as kbe:
+                logging.error("Keyboard interrupt, program has been shut down!")
+
+        self.is_fitted_ = True
+        return True
+
+
     def predict(self, X):
         None
